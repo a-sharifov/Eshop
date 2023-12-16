ae61d5daf3fb04e88f3d9e10324ac9f6fa499d55
Akber <akber.sharifov2004@gmail.com>
2023-12-16 06:24:59 +0400

    change project a new folder src/Services

---
+++ crs/Services/Services.Common/Application/Behaviors/LoggingPipelineBehavior.cs
@@ -0,0 +1,37 @@
+﻿namespace Services.Common.Application.Behaviors;
+
+public sealed class LoggingPipelineBehavior<TRequest, TResponse>
+    (ILogger<LoggingPipelineBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TRequest>
+    where TResponse : Result
+{
+    private readonly ILogger<LoggingPipelineBehavior<TRequest, TResponse>> _logger = logger;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation(
+            "Handling {@RequestName}, {@DateTimeUtcNow}",
+            typeof(TRequest).Name,
+            DateTime.UtcNow);
+
+        var response = await next();
+
+        if(response.IsSuccess)
+        {
+            _logger.LogInformation(
+                "Handled {@RequestName}, {@DateTimeUtcNow}",
+                typeof(TRequest).Name,
+                DateTime.UtcNow);
+
+            return response;
+        }
+
+        _logger.LogError(
+            "Handled {@RequestName}, {@DateTimeUtcNow} error",
+            typeof(TRequest).Name,
+            DateTime.UtcNow);
+
+        return response;
+    }
+}
