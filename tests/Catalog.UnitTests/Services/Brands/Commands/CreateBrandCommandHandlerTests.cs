1ff43363f95f40d301797f5840e1c22597c60a98
Akber <akber.sharifov2004@gmail.com>
2024-01-06 18:28:02 +0400

    Add fail unit test

--- tests/Catalog.UnitTests/Services/Brands/Commands/CreateBrandCommandHandlerTests.cs
+++ tests/Catalog.UnitTests/Services/Brands/Commands/CreateBrandCommandHandlerTests.cs
@@ -2,22 +2,28 @@
 
 public sealed class CreateBrandCommandHandlerTests
 {
-
     //[Fact]
     //public async Task CreateBrandHandler_ShouldReturnSuccessResult()
     //{
-    //    // Arrange
-    //    var brandRepository = new Mock<IBrandRepository>();
-    //    var unitOfWork = new Mock<IUnitOfWork>();
-    //    var command = new CreateBrandCommand("Test", "Test");
-    //    var validator = new CreateBrandValidator();
-    //    var handler = new CreateBrandCommandHandler(unitOfWork.Object, brandRepository.Object, validator);
+    //    //// Arrange
+    //    //var brandRepository = new Mock<IBrandRepository>();
+    //    //var unitOfWork = new Mock<IUnitOfWork>();
+    //    //var command = new CreateBrandCommand("Test", "Test");
+    //    //var validator = new CreateBrandValidator();
+    //    //var handler = new CreateBrandCommandHandler(unitOfWork.Object, brandRepository.Object, validator);
 
-    //    // Act
-    //    var result = await handler.Handle(command);
+    //    //// Act
+    //    //var result = await handler.Handle(command);
 
     //    // Assert
-    //    result.IsSuccess.Should().BeTrue();
+    //    return 
     //}
+
+    [Fact]
+    public void Test1()
+    {
+        int a = 4;
+        a.Should().Be(2);
+    }
 
 }
