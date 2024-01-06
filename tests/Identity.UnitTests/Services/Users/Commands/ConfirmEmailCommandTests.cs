35e564d0771468eb84e8725ddde26ffd71bd65a2
Akber <akber.sharifov2004@gmail.com>
2024-01-07 00:57:27 +0400

    Add more logic to Unit Tests.

--- tests/Identity.UnitTests/Services/Users/Commands/ConfirmEmailCommandTests.cs
+++ tests/Identity.UnitTests/Services/Users/Commands/ConfirmEmailCommandTests.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Identity.UnitTests.Services.Users.Commands;
 
-namespace Identity.UnitTests.Services.Users.Commands
+public sealed class ConfirmEmailCommandTests
 {
-    internal class ConfirmEmailCommandTests
+    [Fact]
+    public void Test1()
     {
+        int a = 4;
+        a.Should().Be(4);
     }
 }
